1a1eb70e68968da0fd9a7e3b3fa7f462e76bb641
SeasonedPorks <yanawarood.yk@gmail.com>
2022-08-04 12:27:17 +0700

    1.6

--- src/views/event/EventEditView.vue
+++ src/views/event/EventEditView.vue
@@ -1,9 +1,25 @@
 <template>
   <p>Edit the event here</p>
+  <button @click="Edit">EDIT ME</button>
 </template>
 
 <script>
 export default {
-  props: ['id', 'event']
+  props: ['id', 'event'],
+  inject: ['GStore'],
+  methods: {
+    Edit() {
+      this.GStore.flashMessage =
+        'You are succesfully Edited ' + this.event.title + 'Page'
+      setTimeout(() => {
+        //after 3 sec remove it
+        this.GStore.flashMessage = ''
+      }, 3000)
+      this.$router.push({
+        name: 'EventDetails',
+        params: { id: this.event.id }
+      })
+    }
+  }
 }
 </script>
--- src/views/event/EventRegisterView.vue
+++ src/views/event/EventRegisterView.vue
@@ -1,9 +1,31 @@
 <template>
   <p>Registration form here</p>
+  <button @click="register">REGISTER ME</button>
 </template>
 
 <script>
 export default {
-  props: ['id', 'event']
+  props: ['id', 'event'],
+  inject: ['GStore'],
+  methods: {
+    register() {
+      //assuming succesful API call to register them
+      console.log('register happen')
+      this.GStore.flashMessage =
+        'You are succesfully registered for ' + this.event.title
+      setTimeout(() => {
+        //after 3 sec remove it
+        this.GStore.flashMessage = ''
+      }, 3000)
+
+      //set a flash message to appear on the next page loaded which says
+      //you are successfully registered for + this.event.title
+
+      this.$router.push({
+        name: 'EventDetails',
+        params: { id: this.event.id }
+      })
+    }
+  }
 }
 </script>
